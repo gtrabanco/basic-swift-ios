d8cfca9d51f9d0ca3810ad5953daa1e0b7afeded
Gabriel Trabanco Llano <gtrabanco@fwok.org>
2015-11-25 02:14:12 +0100

    Class 02

---
+++ 02 - Optionals.playground/Contents.swift
@@ -0,0 +1,48 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+//: Optionals: a box to store a nil
+// ?
+var newBoxStringOptional:String?
+//var newStringNotOptional: String //Error Sponsored by Apple
+
+//newBoxStringOptional = "Hello world!" //This run
+//All go packaged in an optional the "Hello world!" and the first nil. They are not going alone like any other String or Nil (Nil is not valid in Swift)
+
+
+//How to know if the box is empty
+if newBoxStringOptional == nil {
+    print("Do not get anything! IS EMPTY!");
+}
+
+print(newBoxStringOptional) //Here you can show that the string is in an Optional
+
+//Desempaquetar (Spanish) or Unwrap (English)
+//Two ways secure or unsecure (sometimes we want that warning!)
+
+//Secure way
+//You only get inside if the var is no nil
+if let text = newBoxStringOptional {
+    print("That you have inside was \(text)")
+}
+//here the same, just to watch you can use the same name
+if let newBoxStringOptional = newBoxStringOptional {
+    print("That you have inside was \(newBoxStringOptional)")
+}
+
+
+//Other secure way: Known as optional chaining (? after the var)
+// is the same as "if let"
+newBoxStringOptional = "Hello world!"
+newBoxStringOptional?.hasPrefix("Hello") //If not nil run the method
+
+
+
+//Unsecure and in the rought way (!)
+print("Unsecure way \(newBoxStringOptional!) only runs if the var has value if not will throw an error (comment line 13 & 39 to watch it).")
+
+
---
+++ StarWarsVII/StarWarsVII/StarWarsCharacter.swift
@@ -0,0 +1,23 @@
+//
+//  StarWarsCharacter.swift
+//  StarWarsVII
+//
+//  Created by Gabriel Trabanco Llano on 24/11/15.
+//  Copyright © 2015 Gabriel Trabanco Llano. All rights reserved.
+//
+
+import UIKit
+
+class StarWarsCharacter {
+    
+    //MARK: - Properties
+    let firstName    : String = ""
+    let lastName     : String = ""
+    let alias        : String = ""
+//    let photo        : UIImage
+//    let url          : NSURL
+    let affilitation : String = ""
+    
+    
+    
+}
